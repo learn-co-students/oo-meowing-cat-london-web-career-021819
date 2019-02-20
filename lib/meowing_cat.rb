ee48caed18eb1c8f386e4fa23ded90809815ff3d
louieogrady <l.ogrady@gmail.com>
2019-02-20 10:18:09 +0000

    finished

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+class Cat
+
+def initialize # when using attr_accessor, the initialize method does not need an argument as the attr_accessor does this instead
+  @name = name
+end
+
+attr_accessor :name
+
+def meow
+  puts "meow!"
+end
+
+
+end
